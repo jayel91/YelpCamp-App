1a9a30d99b2a6ba4db48f7cc7300eafdbcdde2b0
Josip Lučić <jayel0809@gmail.com>
2018-03-26 18:55:04 +0000

    update dburl v2

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ var commentRoutes = require("./routes/comments"),
     indexRoutes = require("./routes/index");
 
 mongoose.connect(process.env.DATABASEURL);
-//mongoose.connect("mongodb://jayel:ugie1Ohd@ds123799.mlab.com:23799/yelpcampcro");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
